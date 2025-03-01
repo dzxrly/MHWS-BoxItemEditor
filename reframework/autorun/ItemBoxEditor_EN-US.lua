85b71b0d4a5a5ed51b89abac54f47d5e70063d28
Egg Targaryen <dzxrly2@gmail.com>
2025-03-01 10:19:28 +0800

    :bookmark: 发布正式版

--- reframework/autorun/ItemBoxEditor_EN-US.lua
+++ reframework/autorun/ItemBoxEditor_EN-US.lua
@@ -2,8 +2,8 @@
 -- https://github.com/dzxrly/MHWS-BoxItemEditor
 -- MIT License
 -- For Monster Hunter: Wilds
-local INTER_VERSION = "v0.9"
-local MAX_VERSION = "1.0.0.0"
+local INTER_VERSION = "v1.0"
+local MAX_VERSION = "1.0.1.0"
 local I18N = {
     windowTitle = "ItemBox Editor",
     compatibleWarning = "[Warning] Your game version is NOT compatible with this mod: ",
